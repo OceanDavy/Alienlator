7958561480bc89d604a5165d625373e49eecd7f2
Cinco <69429618+OceanDavy@users.noreply.github.com>
2020-09-08 14:56:37 -0700

    Add files via upload

---
+++ Alienlator.js
@@ -0,0 +1,19 @@
+//Each function changes the color of the calculator
+
+function changeColorA(){
+    document.getElementById("title").style.color=" #2ecc71 ";
+    document.getElementById("alienlator").style.background="linear-gradient(90deg, #2ecc71 50%, #28b463 50%)";
+    document.getElementById("equal").style.background="greenyellow ";
+}
+function changeColorB(){
+    document.getElementById("title").style.color=" #f1c40f ";
+    document.getElementById("alienlator").style.background="linear-gradient(90deg, #f1c40f 50%, #d4ac0d 50%)";
+    document.getElementById("equal").style.background=" #ec7063 ";
+}
+function changeColorC(){
+    document.getElementById("title").style.color=" #3498db";
+    document.getElementById("alienlator").style.background="linear-gradient(90deg, #3498db 50%, #2874a6 50%)";
+    document.getElementById("equal").style.background=" #a569bd ";
+}
+
+
